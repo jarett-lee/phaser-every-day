521495a6511f5f59442eab7d85993fd2ff945e44
Jarett Lee <jarett.lee.pi@gmail.com>
2020-02-22 22:31:15 -0500

    Fix long typescript Phaser.Scene casts

--- src/projects/tic-tac-toe/scripts/index.ts
+++ src/projects/tic-tac-toe/scripts/index.ts
@@ -1,5 +1,35 @@
 import Phaser from "phaser";
 
+class SimpleScene extends Phaser.Scene {
+    preload(): void {
+        this.load.setBaseURL("http://labs.phaser.io");
+
+        this.load.image("sky", "assets/skies/space3.png");
+        this.load.image("logo", "assets/sprites/phaser3-logo.png");
+        this.load.image("red", "assets/particles/red.png");
+    }
+
+    create(): void {
+        this.add.image(400, 300, "sky");
+
+        const particles = this.add.particles("red");
+
+        const emitter = particles.createEmitter({
+            speed: 100,
+            scale: {start: 1, end: 0},
+            blendMode: "ADD",
+        });
+
+        const logo = this.physics.add.image(400, 100, "logo");
+
+        logo.setVelocity(100, 200);
+        logo.setBounce(1, 1);
+        logo.setCollideWorldBounds(true);
+
+        emitter.startFollow(logo);
+    }
+}
+
 class SimpleGame {
     game: Phaser.Game;
 
@@ -12,41 +42,12 @@ class SimpleGame {
                 default: "arcade",
                 arcade: {gravity: {y: 200}},
             },
-            scene: {
-                preload: this.preload,
-                create: this.create,
-            },
         };
 
         this.game = new Phaser.Game(config);
-    }
 
-    preload(): void {
-        (this as unknown as Phaser.Scene).load.setBaseURL("http://labs.phaser.io");
-
-        (this as unknown as Phaser.Scene).load.image("sky", "assets/skies/space3.png");
-        (this as unknown as Phaser.Scene).load.image("logo", "assets/sprites/phaser3-logo.png");
-        (this as unknown as Phaser.Scene).load.image("red", "assets/particles/red.png");
-    }
-
-    create(): void {
-        (this as unknown as Phaser.Scene).add.image(400, 300, "sky");
-
-        const particles = (this as unknown as Phaser.Scene).add.particles("red");
-
-        const emitter = particles.createEmitter({
-            speed: 100,
-            scale: {start: 1, end: 0},
-            blendMode: "ADD",
-        });
-
-        const logo = (this as unknown as Phaser.Scene).physics.add.image(400, 100, "logo");
-
-        logo.setVelocity(100, 200);
-        logo.setBounce(1, 1);
-        logo.setCollideWorldBounds(true);
-
-        emitter.startFollow(logo);
+        this.game.scene.add("scene", SimpleScene);
+        this.game.scene.start("scene");
     }
 }
 
