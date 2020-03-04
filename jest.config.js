dbbb041fc09966b5b192c96edb35c5400782061a
Jarett Lee <jarett.lee.pi@gmail.com>
2020-03-04 18:44:54 -0500

    Add jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    roots: ["<rootDir>/src"],
+    moduleNameMapper: {"^lib/(.*)$": "<rootDir>/lib/$1"},
+    testMatch: [
+        "**/__tests__/**/*.+(ts|tsx|js)",
+        "**/?(*.)+(spec|test).+(ts|tsx|js)",
+    ],
+    transform: {"^.+\\.(ts|tsx)$": "ts-jest"},
+};
